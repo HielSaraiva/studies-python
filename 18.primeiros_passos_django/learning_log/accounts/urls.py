ebbb71ab14c0b14bb5fa778166d12e1ebb4e5751
Hiel Saraiva <hielsaraiva11.hs@gmail.com>
2025-01-09 14:48:45 -0300

    feat: implementing login page

---
+++ 18.primeiros_passos_django/learning_log/accounts/urls.py
@@ -0,0 +1,10 @@
+"""Define padrões de URL para contas"""
+
+from django.urls import path, include
+
+app_name = 'accounts'
+urlpatterns = [
+    # Inclui URLs de autenticação default
+    path('', include('django.contrib.auth.urls')),
+    
+]
--- 18.primeiros_passos_django/learning_log/ll_project/settings.py
+++ 18.primeiros_passos_django/learning_log/ll_project/settings.py
@@ -130,3 +130,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Minhas configurações
+LOGIN_REDIRECT_URL = 'learning_logs:index'
