2b8a056e2f6e719b93d7a2fcec0aa16bd406d247
Hiel Saraiva <hielsaraiva11.hs@gmail.com>
2025-03-18 10:10:17 -0300

    fix: adicionando estudos sobre tupla

---
+++ 03.introducao_tipos_dados/tuplas.py
@@ -0,0 +1,19 @@
+# Introdução às tuplas
+
+# Criando tupla
+minha_tupla = ('maçã', 'banana', 'cereja')
+print(minha_tupla)
+
+# Printando elemento a elemento
+for i in minha_tupla:
+    print(i)
+
+print(len(minha_tupla))
+
+# Criando uma tupla com apenas um item
+tupla1 = ('hiel',)
+print(tupla1)
+
+# ALgumas operações
+print(minha_tupla.count('maçã'))
+print(minha_tupla.index('banana'))
